003693e91fc5519eb62cd76ce1856a93ae34c4e0
cavidkazimzada <cavid.kazimov.98@bk.ru>
2024-08-04 18:43:43 +0400

    model configured

---
+++ src/main/java/com/example/newsapp/model/News.java
@@ -0,0 +1,25 @@
+package com.example.newsapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+@Setter
+@Getter
+@Entity
+public class News {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String headline;
+    private String description;
+    private LocalDateTime publicationTime;
+
+}

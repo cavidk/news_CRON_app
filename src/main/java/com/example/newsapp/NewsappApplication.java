633981282e179db577664cf9e0211440122d1c6e
cavidkazimzada <cavid.kazimov.98@bk.ru>
2024-08-05 11:24:30 +0400

    up

--- src/main/java/com/example/newsapp/NewsappApplication.java
+++ src/main/java/com/example/newsapp/NewsappApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NewsappApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(NewsappApplication.class, args);
 	}

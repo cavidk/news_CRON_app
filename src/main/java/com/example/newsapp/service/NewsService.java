a52f3ce853001e1beaa20212dfe52db94e32972e
cavidkazimzada <cavid.kazimov.98@bk.ru>
2024-08-05 11:23:05 +0400

    service added

---
+++ src/main/java/com/example/newsapp/service/NewsService.java
@@ -0,0 +1,31 @@
+package com.example.newsapp.service;
+
+import com.example.newsapp.model.News;
+import com.example.newsapp.repository.NewsRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class NewsService {
+    private final NewsRepository newsRepository;
+
+    public NewsService(NewsRepository newsRepository) {
+        this.newsRepository = newsRepository;
+    }
+
+    public List<News> getNews(LocalDateTime start, LocalDateTime end) {
+        return newsRepository.findByPublicationTimeBetween(start, end);
+    }
+
+    public void saveNews(News news) {
+        newsRepository.save(news);
+    }
+
+    public void deleteOldNews() {
+        LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
+        newsRepository.deleteByPublicationTimeBefore(yesterday);
+    }
+
+}
